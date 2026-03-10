0f80b4c2bdbf87491ec6309d91b0af7196c0827a
Vitor Zavan <vitorzavanj@gmail.com>
2026-03-10 00:22:49 -0300

    voltando a ativa

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from time import sleep
 
 from pages.initial_page import InitialPage
+from pages.login_fail_challeng import LoginFailChallenge
 
 load_dotenv()
 
@@ -11,6 +12,11 @@ load_dotenv()
 def second_challenge():
     url = 'https://practicetestautomation.com/'
     selector_challenge_one = '#menu-item-20'
+    test_login_page = 'a[href*="practice-test-login"]'
+    test_login_page = 'a[href*="practice-test-login"]'
+    locator_login = '#username'
+    locator_password = '#password'
+    submit_button = '#submit'
 
     with sync_playwright() as p:
         browser = p.firefox.launch(headless=False)
@@ -18,6 +24,16 @@ def second_challenge():
 
         initial_page = InitialPage(url, selector_challenge_one)
         initial_page.practice_page(page)
+
+        login_fail = LoginFailChallenge(
+            page,
+            test_login_page,
+            locator_login,
+            locator_password,
+            submit_button
+        )
+
+        login_fail.login_page()
 
         sleep(5)
 
--- pages/login_fail_challeng.py
+++ pages/login_fail_challeng.py
@@ -2,5 +2,37 @@ from playwright.sync_api import Page
 
 
 class LoginFailChallenge:
-    def __init__(self):
+    def __init__(
+            self,
+            page: Page,
+            locator_challenge_two: str,
+            user_selector: str,
+            password_selector: str,
+            button_selector: str,
+            timeout: int=4000
+        ):
+
+        self.page = page
+        self.locator_challenge_two = locator_challenge_two
+        self.user_selector = user_selector
+        self.password_selector = password_selector
+        self.button_selector = button_selector
+        self.timeout = timeout
+
+
+    def login_page(self):
+        try:
+            self.page.wait_for_selector(
+                self.locator_challenge_two,
+                timeout = self.timeout
+        )
+            
+            self.page.get_by_role('link', name='Test Login Page').click()
+            print('Log de SUCESSO aqui (Test login page)')
+
+        except Exception as e:
+            print(f'Log de erro aqui {e}')
+
+
+    def test_login_fail(self):
         ...
